084163e054842a4a56ad04869260c3348b54af9f
Hamilton Tumenas Borges <hamiltontubo@gmail.com>
2025-10-03 14:12:21 -0300

    adiconados metodos de delete, put e update de status

--- src/main/java/com/br/h6n/agendador_tarefas/business/services/TarefaService.java
+++ src/main/java/com/br/h6n/agendador_tarefas/business/services/TarefaService.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Service;
 
 import com.br.h6n.agendador_tarefas.business.dto.TarefaDTO;
 import com.br.h6n.agendador_tarefas.business.mapper.TarefaConverter;
+import com.br.h6n.agendador_tarefas.business.mapper.TarefaUpdateConverter;
 import com.br.h6n.agendador_tarefas.infrastructure.entity.TarefaEntity;
 import com.br.h6n.agendador_tarefas.infrastructure.enums.StatusNotificacaoEnum;
+import com.br.h6n.agendador_tarefas.infrastructure.exceptions.ResourceNotFoundException;
 import com.br.h6n.agendador_tarefas.infrastructure.repository.TarefaRepository;
 import com.br.h6n.agendador_tarefas.infrastructure.security.JwtUtil;
 
@@ -19,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 public class TarefaService {
     private final TarefaRepository tarefaRepository;
     private final TarefaConverter tarefaConverter;
+    private final TarefaUpdateConverter tarefaUpdateConverter;
     private final JwtUtil jwtUtil;
 
     public TarefaDTO gravarTarefa(String token, TarefaDTO tarefaDTO) {
@@ -40,5 +43,35 @@ public class TarefaService {
        List<TarefaEntity> listaTarefas = tarefaRepository.findByEmailUsuario(email);
 
        return tarefaConverter.paraListaTarefaDTO(listaTarefas);
+   }
+
+   public void deletaTarefaPorId(String id) {
+        try {
+           tarefaRepository.deleteById(id);
+       } catch (ResourceNotFoundException e) {
+           throw new ResourceNotFoundException("Tarefa não encontrada.", e.getCause());
+       }
+   }
+
+   public TarefaDTO alteraStatus(StatusNotificacaoEnum status, String id) {
+        try {
+            TarefaEntity tarefaEntity = tarefaRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException("Tarefa não encontrada."));
+            tarefaEntity.setStatusNotificacaoEnum(status);
+            return tarefaConverter.paraTarefaDTO(tarefaRepository.save(tarefaEntity));
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException("Erro ao alterar status da tarefa.", e.getCause());
+        }
+   }
+
+   public TarefaDTO updateTarefa(String id, TarefaDTO tarefaDTO) {
+       try {
+           TarefaEntity tarefaEntity = tarefaRepository.findById(id)
+                   .orElseThrow(() -> new ResourceNotFoundException("Tarefa não encontrada."));
+           tarefaUpdateConverter.updateTarefa(tarefaDTO, tarefaEntity);
+           return tarefaConverter.paraTarefaDTO(tarefaRepository.save(tarefaEntity));
+       } catch (ResourceNotFoundException e) {
+           throw new ResourceNotFoundException("Erro ao atualizar tarefa.", e.getCause());
+       }
    }
 }
--- src/main/java/com/br/h6n/agendador_tarefas/controller/TarefaController.java
+++ src/main/java/com/br/h6n/agendador_tarefas/controller/TarefaController.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.br.h6n.agendador_tarefas.business.dto.TarefaDTO;
 import com.br.h6n.agendador_tarefas.business.services.TarefaService;
+import com.br.h6n.agendador_tarefas.infrastructure.enums.StatusNotificacaoEnum;
 
 import lombok.RequiredArgsConstructor;
-
-
 
 @RestController
 @RequestMapping("/tarefas")
@@ -44,4 +47,21 @@ public class TarefaController {
     public ResponseEntity<List<TarefaDTO>> buscarTarefasPorEmail(@RequestHeader("Authorization") String token) {
         return ResponseEntity.ok(tarefaService.buscaTarefasPorEmail(token));
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarTarefa(@PathVariable String id) {
+        tarefaService.deletaTarefaPorId(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<TarefaDTO> alterarStatusNotificacao(@PathVariable String id, @RequestParam StatusNotificacaoEnum status) {
+        return ResponseEntity.ok(tarefaService.alteraStatus(status, id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TarefaDTO> atualizarTarefa(@PathVariable String id, @RequestBody TarefaDTO tarefaDTO) {
+        return ResponseEntity.ok(tarefaService.updateTarefa(id, tarefaDTO));
+    }
+
 }
