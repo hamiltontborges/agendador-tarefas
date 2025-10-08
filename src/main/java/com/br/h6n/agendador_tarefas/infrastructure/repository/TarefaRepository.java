ab7eb32ee6196c616380e95140224e5e20765b29
Hamilton Tumenas Borges <hamiltontubo@gmail.com>
2025-10-08 11:24:55 -0300

    busca de tarefas por periodo e por status pendente

--- src/main/java/com/br/h6n/agendador_tarefas/infrastructure/repository/TarefaRepository.java
+++ src/main/java/com/br/h6n/agendador_tarefas/infrastructure/repository/TarefaRepository.java
@@ -6,10 +6,11 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.br.h6n.agendador_tarefas.infrastructure.entity.TarefaEntity;
+import com.br.h6n.agendador_tarefas.infrastructure.enums.StatusNotificacaoEnum;
 
 public interface TarefaRepository extends MongoRepository<TarefaEntity, String> {
 
-    List<TarefaEntity> findByDataEventoBetween(LocalDateTime start, LocalDateTime end);
+    List<TarefaEntity> findByDataEventoBetweenAndStatusNotificacaoEnum(LocalDateTime start, LocalDateTime end, StatusNotificacaoEnum status);
 
     List<TarefaEntity> findByEmailUsuario(String email);
 
